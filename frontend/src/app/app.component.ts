956a1dff786e27d57a1e812c5e05f08d35dd84cc
caio2983 <caioluna2002@gmail.com>
2024-10-17 18:08:22 -0300

    feat: additional messages field and dialog box template improvements

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,79 +1,95 @@
-import { Component,OnInit,ChangeDetectionStrategy,inject,model,signal } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  inject,
+  model,
+  signal,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SocketService } from './socket.service';
 import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { DialogUserComponent } from './components/dialog-user/dialog-user.component';
-;
-import { MatDialogActions,MAT_DIALOG_DATA,MatDialogClose,MatDialogContent,MatDialogRef,MatDialogTitle,MatDialog, } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import {
+  MatDialogActions,
+  MAT_DIALOG_DATA,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+  MatDialog,
+} from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInput, MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { ColorPicker, ColorPickerModule } from 'primeng/colorpicker';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule,MatFormFieldModule,MatInputModule,FormsModule,MatButton,ColorPickerModule],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButton,
+    ColorPickerModule,
+    MatCardModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'frontend';
-  
-  mensagens : string[] = [];
-  mensagensSubscription : Subscription | undefined;
+
+  mensagens: string[] = [];
+  mensagensSubscription: Subscription | undefined;
 
   color: string = '#6466f1';
   readonly name = model('');
   readonly dialog = inject(MatDialog);
 
-
- 
-
-  constructor(private socketService : SocketService) {
-   
-  }
+  constructor(private socketService: SocketService) {}
 
   ngOnInit() {
     this.socketService.connect();
-   
 
     this.socketService.onMessage();
 
     this.mensagensSubscription = this.socketService.mensagens$.subscribe({
-      next:(messages: string[]) => {
-        this.mensagens= messages
-      }
+      next: (messages: string[]) => {
+        this.mensagens = messages;
+      },
 
-    // inscrição no observable mensagens$ / mensagensSubject que está no services. atualiza o array this.mensagens com a resposta do
-    // observable 
-    })
-    
+      // inscrição no observable mensagens$ / mensagensSubject que está no services. atualiza o array this.mensagens com a resposta do
+      // observable
+    });
 
-    // this.openDialog();
-
+    this.openDialog();
   }
 
   sendMessage(newMessage: string): void {
-  
     this.socketService.sendMessage(newMessage);
-  
   }
 
-  // openDialog(): void {
-  //   const dialogRef = this.dialog.open(DialogUserComponent, {
-  //     data: {name: this.name(), animal: this.color()},
-  //   });
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogUserComponent, {
+      data: { name: this.name(), color: this.color },
+    });
 
-  //   dialogRef.afterClosed().subscribe(result => {
-  //     console.log('The dialog was closed');
-  //     if (result !== undefined) {
-  //       this.color.set(result);
-  //     }
-  //   });
-  // }
+    // dialogRef.afterClosed().subscribe((result) => {
+    //   console.log('The dialog was closed');
+    //   if (result !== undefined) {
+    //     this.color.set(result);
+    //   }
+    // });
+  }
+
+  teste() {
+    console.log(this.color);
+  }
 }
-
--- frontend/src/app/components/dialog-user/dialog-user.component.ts
+++ frontend/src/app/components/dialog-user/dialog-user.component.ts
@@ -1,7 +1,12 @@
-
-import {ChangeDetectionStrategy, Component, inject, model, signal} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  model,
+  signal,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import {
   MAT_DIALOG_DATA,
   MatDialog,
@@ -11,25 +16,36 @@ import {
   MatDialogRef,
   MatDialogTitle,
 } from '@angular/material/dialog';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { User } from '../../interfaces/UserInterface';
 import { ColorPickerModule } from 'primeng/colorpicker';
-
 
 @Component({
   selector: 'app-dialog-user',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule,ColorPickerModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    ColorPickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogClose,
+  ],
   templateUrl: './dialog-user.component.html',
-  styleUrl: './dialog-user.component.scss'
+  styleUrl: './dialog-user.component.scss',
 })
 export class DialogUserComponent {
   readonly dialog = inject(MatDialog);
   color!: any;
+  username!: string;
 
-  constructor() {
- 
+  constructor() {}
+
+  teste() {
+    console.log(this.username);
+    console.log(this.color);
   }
-  
 }
